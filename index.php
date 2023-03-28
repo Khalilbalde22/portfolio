6ff915da9a24c67d2c7434dc7d11d10858b7145b
Kalil Balde <kalilbalde20@gmail.com>
2023-03-28 12:08:24 +0200

    premier commit

--- index.php
+++ index.php
@@ -10,23 +10,21 @@
     <header>
       <div id="accueil" class="container">
 
-        <h1>Khalil</h1>
+        <h1 style = "margin-bottom:-50px">Khalil</h1>
 
-        <nav >
+        <nav>
           <ul>
-          
-            <li><a href="#accueil">Accueil</a></li>
-            <li><a href="#apropos">À propos de moi</a></li>
-            <li><a href="#competances">Mes Competances</a></li>
-            <li><a href="#projects">Mes Projets</a></li>
-            <li><a href="#reference">References</a></li>
-            <li><a href="#contact" class = "btn">Contactez-moi</a></li>
+            <li ><a href="#accueil">Accueil</a></li>
+            <li ><a href="#competances">Mes Competances</a></li>
+            <li ><a href="#projects">Mes Projets</a></li>
+            <li ><a href="#references">References</a></li>
+            <li ><a href="#contact" class = "btn">Contactez-moi</a></li>
           </ul>
           
         </nav>
       </div>
     </header>
-
+<!-- mon profil -->
     <section id="profil">
       <div class="container">
         <div class="title">
@@ -37,81 +35,104 @@
         </div>
       </div>
     </section>
-
-    <section id="apropos">
+<!-- les statistiques  -->
+    <section id="statistique">
       <div class="container">
-        <h2>À propos de moi</h2>
-        <p>Je suis un développeur web avec plus de 5 ans d'expérience dans le domaine. J'aime créer des sites web fonctionnels et élégants qui répondent aux besoins de mes clients.</p>
+          <div class="row">
+              <div class="stats">
+                  <img src="img/icon-client2.png" alt="icon-clients">
+                  <p>10 projets</p>
+              </div>
+              <div class="stats">
+                  <img src="img/icon-projet3.png" alt="icon-projets">
+                  <p>20 projets</p>
+              </div>
+              <div class="stats">
+                  <img src="img/icon-experience2.png" alt="icon-experiences">
+                  <p>5ans experien</p>
+              </div>
+              <div class="stats">
+                  <img src="img/icon-colaboration.png" alt="icon-colaboration">
+                  <p>3 collabs</p>
+              </div>
+          </div>
       </div>
     </section>
-
+<!-- mes competances -->
     <section id="competances">
       <div class="container">
         <div class="couleurbackground">
           <h2>Mes Competances</h2>
           <div class="row">
               <div class="project">
-                  <img src="backgruond.jfif" alt="Projet 1" />
-                  <h3>Projet 1</h3>
+                  <img src="img/icon-competance-html.png" alt="Projet 1" />
+                  <h3>HTML & CSS</h3>
                   <p>Ce projet est un site web e-commerce conçu pour une boutique de vêtements en ligne. J'ai utilisé HTML, CSS et JavaScript pour créer un site web dynamique et facile à utiliser.</p>
               </div>
               <div class="project">
-                  <img src="backgruond.jfif" alt="Projet 2" />
-                  <h3>Projet 2</h3>
+                  <img src="img/icon-competance-bootstrap4.jpg" alt="Projet 3" />
+                  <h3>BOOTSTRAP</h3>
+                  <p>Ce projet est un site web e-commerce conçu pour une boutique de vêtements en ligne. J'ai utilisé HTML, CSS et JavaScript pour créer un site web dynamique et facile à utiliser.</p>
+              </div>
+              <div class="project">
+                  <img src="img/icon-competance-php3.png" alt="Projet 2" />
+                  <h3>PHP</h3>
                   <p>Ce projet est un site web d'actualités conçu pour un journal en ligne. J'ai utilisé WordPress pour créer un site web facile à mettre à jour et à gérer pour l'équipe du journal.</p>
               </div>
               <div class="project">
-                  <img src="backgruond.jfif" alt="Projet 2" />
-                  <h3>Projet 2</h3>
+                  <img src="img/icon-competance-laravel.jpg" alt="Projet 2" />
+                  <h3>Laravel</h3>
                   <p>Ce projet est un site web d'actualités conçu pour un journal en ligne. J'ai utilisé WordPress pour créer un site web facile à mettre à jour et à gérer pour l'équipe du journal.</p>
               </div>
           </div>
         </div>
       </div>
     </section>
-
+<!-- la partie qui presente les projets sur les quels j'ai eu a travailler -->
     <section id="projects">
       <div class="container">
         <div class = "couleurbackground">
           <h2>Mes projets</h2>
           <div class="row">
+
               <div class="project">
-                  <img src="imgProjet2.PNG" alt="Projet 1" />
+                  <img src="img/imgProjet2.PNG" alt="Projet 1" />
                   <h3>Projet 1</h3>
                   <p>Ce projet est un site web e-commerce conçu pour une boutique de vêtements en ligne. J'ai utilisé HTML, CSS et JavaScript pour créer un site web dynamique et facile à utiliser.</p>
               </div>
               <div class="project">
-                  <img src="imgProjet2.PNG" alt="Projet 2" />
+                  <img src="img/imgProjet2.PNG" alt="Projet 2" />
                   <h3>Projet 2</h3>
                   <p>Ce projet est un site web d'actualités conçu pour un journal en ligne. J'ai utilisé WordPress pour créer un site web facile à mettre à jour et à gérer pour l'équipe du journal.</p>
               </div>
               <div class="project">
-                  <img src="imgProjet3.PNG" alt="Projet 3" />
+                  <img src="img/imgProjet3.PNG" alt="Projet 3" />
                   <h3>Projet 3</h3>
                   <p>Ce projet est un site web d'actualités conçu pour un journal en ligne. J'ai utilisé WordPress pour créer un site web facile à mettre à jour et à gérer pour l'équipe du journal.</p>
               </div>
+
           </div>
         </div>
       </div>
     </section>
-
-    <section id="reference">
+<!-- cette partie concerne les references professionnelles -->
+    <section id="references">
       <div class="container">
         <div class = "couleurbackground">
           <h2>Références Professionnelles</h2>
           <div class="row">
               <div class="reference">
-                  <img src="imgProjet2.PNG" alt="Projet 1" />
+                  <img src="img/imgProjet2.PNG" alt="Projet 1" />
                   <h3>Projet 1</h3>
                   <p>Ce projet est un site web e-commerce conçu pour une boutique de vêtements en ligne. J'ai utilisé HTML, CSS et JavaScript pour créer un site web dynamique et facile à utiliser.</p>
               </div>
               <div class="reference">
-                  <img src="imgProjet2.PNG" alt="Projet 2" />
+                  <img src="img/imgProjet2.PNG" alt="Projet 2" />
                   <h3>Projet 2</h3>
                   <p>Ce projet est un site web d'actualités conçu pour un journal en ligne. J'ai utilisé WordPress pour créer un site web facile à mettre à jour et à gérer pour l'équipe du journal.</p>
               </div>
               <div class="reference">
-                  <img src="imgProjet3.PNG" alt="Projet 3" />
+                  <img src="img/imgProjet3.PNG" alt="Projet 3" />
                   <h3>Projet 3</h3>
                   <p>Ce projet est un site web d'actualités conçu pour un journal en ligne. J'ai utilisé WordPress pour créer un site web facile à mettre à jour et à gérer pour l'équipe du journal.</p>
               </div>
@@ -119,7 +140,7 @@
         </div>
       </div>
     </section>
-
+<!-- cette partie et pour la prise de contact et les formulair de contact -->
     <section id="contact">
       <div class="container">
         <div class="couleurbackground">
@@ -159,22 +180,24 @@
         </div>
       </div>
     </section>
-
+<!-- les element du pied de page -->
     <footer>
       <div class="container">
         <div class="row">
             <div class="navfooter">
                 <ul class="ulfooter">
                     <li><a style="text-decoration: none" href="#accueil">Accueil</a></li>
-                    <li><a style="text-decoration: none" href="#apropos">Apropos</a></li>
                     <li><a style="text-decoration: none" href="#competances">Mes competances</a></li>
-                    <li><a style="text-decoration: none" href="#projets">Mes projets</a></li>
+                    <li><a style="text-decoration: none" href="#projects">Mes projets</a></li>
+                    <li><a style="text-decoration: none" href="#references">Référence Professionnelles</a></li>
                     <li><a style="text-decoration: none" href="#contact">contactez-moi</a></li>
                 </ul>
             </div>
         </div>
       </div>
-            <div class="navfooter"><p> &copy; copyrigth 2023 </p></div>
+      <div class="navfooter"><p> &copy; copyright 2023 </p></div>
+<!-- l'inclusion du fichier javaScript -->
+<script src="scrypts.js"></script>
     </footer>
   </body>
 </html>
---
+++ scrypts.js
@@ -0,0 +1,13 @@
+
+var lien = document.querySelector('nav ul li h3');
+var touslien = document.querySelectorAll('.alien');
+
+
+var titre = document.querySelector('.title').textContent;
+alert(titre);
+
+var title = document.title;
+var corps = document.body;
+var links = document.links;
+
+alert('le contenu du : ' + titre + 'la partie body : ' + corps + 'la partie lien : ' + links[0])
